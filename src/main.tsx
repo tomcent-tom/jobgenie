1cb0aeb4f0a83989d129ce1d9d15a5fa431c6e4f
tomcent-tom <tom.hh.evers@gmail.com>
2025-01-14 18:22:45 +1100

    Add posthog tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { PostHogProvider} from 'posthog-js/react'
+
+const posthogKey = import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY;
+const posthogHost = import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST;
+
+const options = {
+  api_host: posthogHost,
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider 
+      apiKey={posthogKey}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
